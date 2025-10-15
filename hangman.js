2b727df7c4d3c82cf6d05012c87889c8b3dde154
David Cano <davecano.244@gmail.com>
2025-10-15 10:42:39 -0400

    added possible words

---
+++ hangman.js
@@ -0,0 +1,5 @@
+var POSSIBLE_WORDS = ["obdurate", "versatimilitude", "defenestrate", "obsequious", "dissonant","toady","idempotent"];
+
+var randomIndex = parseInt(Math.random()) * POSSIBLE_WORDS.length;
+
+var word = POSSIBLE_WORDS[randomIndex];
