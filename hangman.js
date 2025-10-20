4e7f365041c5840f4324ccce97bf0a7b466cb33b
David Cano <davecano.244@gmail.com>
2025-10-20 10:34:26 -0400

    changes for lines

--- hangman.js
+++ hangman.js
@@ -1,16 +1,40 @@
-var POSSIBLE_WORDS = ["obdurate", "versatimilitude", "defenestrate", "obsequious", "dissonant", "toady", "idempotent"];
+var POSSIBLE_WORDS = ["obdurate", "verisimilitude", "defenestrate", "obsequious", "dissonant", "toady", "idempotent"];
 
 
 var word = "";
+var guesses = "";
 
 function newGame() {
-    var randomIndex = parseInt(Math.random()) * POSSIBLE_WORDS.length;
-    word = POSSIBLE_WORDS[randomIndex];
+    var randomIndex = parseInt(Math.random() * POSSIBLE_WORDS.length);
+    word = POSSIBLEWORDS[randomIndex];
 
+    guesses = "";
+    updatePage();
+}
+function guessLetter() {
+
+    var input = document.getElementById("guess");
+    var letter = input.value;
+    guesses += letter;
+
+    updatePage();
+}
+
+function updatePage() {
     var clueString = "";
+
     for (var i = 0; i < word.length; i++) {
-        clueString += "_ ";
+        var currentLetter = word.charAt(i);
+        if(guesses.indexOf(currentLetter) >= 0){
+            clueString += currentLetter + " ";
+        } else {
+            clueString += " ";
+        }
+
     }
     var clue = document.getElementById("clue");
     clue.innerHTML = clueString;
+
+    var guessArea = document.getElementById("guesses");
+    guessArea.innerHTML = "Guessed Letters: " + guesses;
 }
